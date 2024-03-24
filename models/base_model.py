ba154af54dd47729297e3930f2c32050df64fe2e
lokmane rouijel <lokmane9919@gmail.come>
2024-03-23 21:55:36 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
-import uuid
+"""
+Class BaseModel defines all common attributes/methods for other classes
+"""
 from datetime import datetime
+from uuid import uuid4
+import models
+
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """Class BaseModel"""
+    def __init__(self, *args, **kwargs):
+        """constructor of a BaseModel"""
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    my_form = "%Y-%m-%dT%H:%M:%S.%f"
+                    setattr(self, key, datetime.strptime(value, my_form))
+                elif k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """should print: [<class name>] (<self.id>) <self.__dict__>"""
+        name = self.__class__.__name__
+        id = self.id
+        my_dict = str(self.__dict__)
+        return ("[" + name + "] (" + id + ") " + my_dict)
 
     def save(self):
+        """updates the public instance attribute updated_at with
+        the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        obj_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        return obj_dict
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        keys = self.__dict__.copy()
+        keys['__class__'] = self.__class__.__name__
+        keys["created_at"] = keys["created_at"].isoformat()
+        keys["updated_at"] = keys["updated_at"].isoformat()
+        return keys
