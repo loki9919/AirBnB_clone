4bd210a9b442ec500fb000015eb449903b55f84e
lokmane rouijel <lokmane@DESKTOP-I5AHK89.localdomain>
2024-03-30 03:45:00 +0000

    console

--- console.py
+++ console.py
@@ -48,7 +48,7 @@ class HBNBCommand(cmd.Cmd):
 
     def precmd(self, line):
         """this method is called after the line has been input but before
-            it has been interpreted"""
+            it has been interprete"""
         if "." in line:
             args = re.split(r'\.|\(|\)|, |{|}|: ', line)
             line = args[1] + " " + args[0]
